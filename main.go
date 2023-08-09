f88bb658112af574515db31d1d013f7794390dc7
mzzsfy <mzzsfy@163.com>
2023-08-09 17:45:19 +0800

    update

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
     _ "embed"
     "flag"
     _ "github.com/mzzsfy/go-gen/gin"
+    "github.com/mzzsfy/go-gen/register"
 )
+
+var genType = flag.String("genType", "gin-router", "生成类型,默认为gin-router")
 
 func main() {
     flag.Parse()
+    register.Do(*genType)
 }
