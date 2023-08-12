d97bb595fdb56c3a677197e166e03298aba1d4a7
mzzsfy <mzzsfy@163.com>
2023-08-12 18:24:19 +0800

    添加参数打印

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
     _ "embed"
     "flag"
+    "fmt"
     _ "github.com/mzzsfy/go-gen/enhance"
     _ "github.com/mzzsfy/go-gen/gin"
     "github.com/mzzsfy/go-gen/register"
+    "os"
 )
 
 var genType = flag.String("genType", "gin-router", "生成类型,默认为gin-router")
 
 func main() {
     flag.Parse()
+    cwd, err := os.Getwd()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("cwd = %s\n", cwd)
+    fmt.Printf("os.Args = %#v\n", os.Args)
     register.Do(*genType)
 }
